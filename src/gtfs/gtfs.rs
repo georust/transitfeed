545f3f55f53595563f5e692740b3a556fa08330b
Tom Burdick <thomas.burdick@gmail.com>
2016-08-23 15:48:34 -0500

    refactor: break out gtfs to a module

---
+++ src/gtfs/gtfs.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+use std::io::{BufRead, Read, Seek};
+use zip::ZipArchive;
+use quick_csv::Csv;
+use transit::Transit;
+use gtfs::error::GtfsError;
+use gtfs::{AgencyIterator, RouteIterator, StopIterator};
+
+pub struct GTFS<R: Read+Seek> {
+    raw: R,
+    archive: ZipArchive<R>,
+}
+
+
+//impl<'a, E: Error, R: Read+Seek+'a, B: BufRead> Transit<'a, E> for GTFS<R> {
+//    type AgencyIterator = AgencyIterator<'a, B>;
+//
+//    fn agencies(&self) -> AgencyIterator {
+//        let csv = Csv::from_reader(self.archive.by_name("agency.txt").unwrap());
+//        AgencyIterator::new(csv)
+//    }
+//
+//}
--- src/gtfs/mod.rs
+++ src/gtfs/mod.rs
@@ -1,12 +1,14 @@
 mod error;
 #[macro_use]
 mod parse;
+mod gtfs;
 mod agencies;
 mod routes;
 mod stops;
 mod stop_times;
 mod trips;
 
+pub use gtfs::gtfs::GTFS;
 pub use gtfs::agencies::AgencyIterator;
 pub use gtfs::routes::RouteIterator;
 pub use gtfs::stops::StopIterator;
