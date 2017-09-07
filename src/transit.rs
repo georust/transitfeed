87f443520d1078c5df32c3297c4985f677d03489
Michael Edwards <medwards@walledcity.ca>
2017-09-07 22:04:08 +0200

    Reorder transit structs (by order in reference), impl Debug

--- src/transit.rs
+++ src/transit.rs
@@ -10,6 +10,7 @@ pub trait Transit<'a, E: Error> {
 }
 
 /// Agency
+#[derive(Debug)]
 pub struct Agency {
     pub agency_id: Option<String>,
     pub agency_name: String,
@@ -22,6 +23,7 @@ pub struct Agency {
 }
 
 /// Calendar
+#[derive(Debug)]
 pub struct Calendar {
     pub service_id: String,
     pub monday: bool,
@@ -36,25 +38,144 @@ pub struct Calendar {
 }
 
 /// ExceptionType for `CalendarDate`
+#[derive(Debug)]
 pub enum ExceptionType {
     ServiceAdded,
     ServiceRemoved,
 }
 
 /// CalendarDate
+#[derive(Debug)]
 pub struct CalendarDate {
     pub service_id: String,
     pub date: NaiveDate,
     pub exception_type: ExceptionType
 }
 
+/// PaymentMethod for `FareAttribute`
+#[derive(Debug)]
+pub enum PaymentMethod {
+    PaidOnboard,
+    PaidBefore,
+}
+
+/// Tranfers for `FareAttribute`
+#[derive(Debug)]
+pub enum Transfers {
+    None,
+    TransferOnce,
+    TransferTwice,
+    Unlimited,
+}
+
+/// FareAttribute
+#[derive(Debug)]
+pub struct FareAttribute {
+    pub fare_id: String,
+    pub price: f64,
+    pub currency_type: String,
+    pub payment_method: PaymentMethod,
+    pub transfers: Transfers,
+    pub transfer_duration: Duration,
+}
+
+/// FareRule
+/// origin, destination, and contains reference a zone_id from stops
+#[derive(Debug)]
+pub struct FareRule {
+    pub fare_id: String,
+    pub route_id: Option<String>,
+    pub origin_id: Option<String>,
+    pub destination_id: Option<String>,
+    pub contains_id: Option<String>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TimeOffset {
+    pub hours: u32,
+    pub minutes: u32,
+    pub seconds: u32,
+}
+
+impl TimeOffset {
+    pub fn from_hms(hours: u32, minutes: u32, seconds: u32) -> TimeOffset {
+        TimeOffset {
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+        }
+    }
+
+    pub fn duration(&self) -> Duration {
+        Duration::hours(self.hours as i64)
+            + Duration::minutes(self.minutes as i64)
+            + Duration::seconds(self.seconds as i64)
+    }
+}
+
+/// exact_times for Frequency
+#[derive(Debug)]
+pub enum FrequencyAccuracy {
+    Approximate,
+    Exact
+}
+
+/// Frequency
+#[derive(Debug)]
+pub struct Frequency {
+    pub trip_id: String,
+    pub start_time: TimeOffset,
+    pub end_time: TimeOffset,
+    pub headway_secs: u64,
+    pub exact_times: FrequencyAccuracy,
+}
+
+/// RouteType
+#[derive(Debug)]
+pub enum RouteType {
+    LightRail,
+    Subway,
+    Rail,
+    Bus,
+    Ferry,
+    CableCar,
+    Gondola,
+    Funicular,
+}
+
+/// Route
+#[derive(Debug)]
+pub struct Route {
+    pub route_id: String,
+    pub agency_id: Option<String>,
+    pub route_short_name: String,
+    pub route_long_name: String,
+    pub route_desc: Option<String>,
+    pub route_type: RouteType,
+    pub route_url: Option<String>,
+    pub route_color: Option<String>,
+    pub route_text_color: Option<String>,
+}
+
+/// Shape
+#[derive(Debug)]
+pub struct Shape {
+    pub shape_id: String,
+    pub shape_pt_lat: f64,
+    pub shape_pt_lon: f64,
+    pub shape_pt_sequence: u64,
+    pub shape_dist_traveled: f64,
+}
+
 /// Location Type
+#[derive(Debug)]
 pub enum LocationType {
     Stop,
     Station,
 }
 
 /// Wheelchair Boarding
+#[derive(Debug)]
 pub enum WheelchairBoarding {
     NoInformation,
     SomeAccessibility,
@@ -62,6 +183,7 @@ pub enum WheelchairBoarding {
 }
 
 /// Stop
+#[derive(Debug)]
 pub struct Stop {
     pub stop_id: String,
     pub stop_code: Option<String>,
@@ -75,74 +197,6 @@ pub struct Stop {
     pub parent_station: Option<String>,
     pub stop_timezone: Option<String>,
     pub wheelchair_boarding: WheelchairBoarding,
-}
-
-/// RouteType
-pub enum RouteType {
-    LightRail,
-    Subway,
-    Rail,
-    Bus,
-    Ferry,
-    CableCar,
-    Gondola,
-    Funicular,
-}
-
-/// Route
-pub struct Route {
-    pub route_id: String,
-    pub agency_id: Option<String>,
-    pub route_short_name: String,
-    pub route_long_name: String,
-    pub route_desc: Option<String>,
-    pub route_type: RouteType,
-    pub route_url: Option<String>,
-    pub route_color: Option<String>,
-    pub route_text_color: Option<String>,
-}
-
-/// Wheelchair Accessible
-pub enum WheelchairAccessible {
-    NoInformation,
-    SomeAccessibility,
-    NoAccessibility,
-}
-
-/// Bikes Allowed
-pub enum BikesAllowed {
-    NoInformation,
-    SomeBikes,
-    NoBikes,
-}
-
-/// Trip
-pub struct Trip {
-    pub route_id: String,
-    pub service_id: String,
-    pub trip_id: String,
-    pub trip_headsign: Option<String>,
-    pub trip_short_name: Option<String>,
-    pub direction_id: Option<String>,
-    pub block_id: Option<String>,
-    pub shape_id: Option<String>,
-    pub wheelchair_accessible: WheelchairAccessible,
-    pub bikes_allowed: BikesAllowed,
-}
-
-/// exact_times for Frequency
-pub enum FrequencyAccuracy {
-    Approximate,
-    Exact
-}
-
-/// Frequency
-pub struct Frequency {
-    pub trip_id: String,
-    pub start_time: TimeOffset,
-    pub end_time: TimeOffset,
-    pub headway_secs: u64,
-    pub exact_times: FrequencyAccuracy,
 }
 
 /// PickupType for `StopTime`
@@ -170,30 +224,6 @@ pub enum Timepoint {
     Exact,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TimeOffset {
-    pub hours: u32,
-    pub minutes: u32,
-    pub seconds: u32,
-}
-
-impl TimeOffset {
-    pub fn from_hms(hours: u32, minutes: u32, seconds: u32) -> TimeOffset {
-        TimeOffset {
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds,
-        }
-    }
-
-    pub fn duration(&self) -> Duration {
-        Duration::hours(self.hours as i64)
-            + Duration::minutes(self.minutes as i64)
-            + Duration::seconds(self.seconds as i64)
-    }
-}
-
-
 /// StopTime
 #[derive(Debug)]
 pub struct StopTime {
@@ -209,46 +239,33 @@ pub struct StopTime {
     pub timepoint: Timepoint,
 }
 
-/// PaymentMethod for `FareAttribute`
-pub enum PaymentMethod {
-    PaidOnboard,
-    PaidBefore,
+/// Wheelchair Accessible
+#[derive(Debug)]
+pub enum WheelchairAccessible {
+    NoInformation,
+    SomeAccessibility,
+    NoAccessibility,
 }
 
-/// Tranfers for `FareAttribute`
-pub enum Transfers {
-    None,
-    TransferOnce,
-    TransferTwice,
-    Unlimited,
+/// Bikes Allowed
+#[derive(Debug)]
+pub enum BikesAllowed {
+    NoInformation,
+    SomeBikes,
+    NoBikes,
 }
 
-
-/// FareAttribute
-pub struct FareAttribute {
-    pub fare_id: String,
-    pub price: f64,
-    pub currency_type: String,
-    pub payment_method: PaymentMethod,
-    pub transfers: Transfers,
-    pub transfer_duration: Duration,
-}
-
-/// FareRule
-/// origin, destination, and contains reference a zone_id from stops
-pub struct FareRule {
-    pub fare_id: String,
-    pub route_id: Option<String>,
-    pub origin_id: Option<String>,
-    pub destination_id: Option<String>,
-    pub contains_id: Option<String>,
-}
-
-/// Shape
-pub struct Shape {
-    pub shape_id: String,
-    pub shape_pt_lat: f64,
-    pub shape_pt_lon: f64,
-    pub shape_pt_sequence: u64,
-    pub shape_dist_traveled: f64,
+/// Trip
+#[derive(Debug)]
+pub struct Trip {
+    pub route_id: String,
+    pub service_id: String,
+    pub trip_id: String,
+    pub trip_headsign: Option<String>,
+    pub trip_short_name: Option<String>,
+    pub direction_id: Option<String>,
+    pub block_id: Option<String>,
+    pub shape_id: Option<String>,
+    pub wheelchair_accessible: WheelchairAccessible,
+    pub bikes_allowed: BikesAllowed,
 }
